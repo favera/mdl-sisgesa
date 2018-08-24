264b73ca5d1e95ef9fd1d35ec73ce83b385ce189
fvera <faa88ver@gmail.com>
2018-08-24 17:03:56 -0300

    ajustes

--- src/components/attendance/Attendance.vue
+++ src/components/attendance/Attendance.vue
@@ -206,7 +206,14 @@ export default {
     },
     //calcula el retraso del funcionario en caso de que haya uno.
     getDelay(employeeId, attEntry, attExit, date) {
-      var workingHours, workedHours, result, entryTime, exitTime, holiday, openHours, closeHours;
+      var workingHours,
+        workedHours,
+        result,
+        entryTime,
+        exitTime,
+        holiday,
+        openHours,
+        closeHours;
       //si la fecha es string format a date
       if (typeof date === "string") {
         date = new Date(date);
@@ -224,24 +231,28 @@ export default {
       // console.log(date);
       // console.log("Verificando si es Domingo", date.getDay === 0);
       //calculo basado en el horario de apertura del local, horaApertura - horaEntrada
-      this.employees.filter(employee=> {
-        if(employee._id === employeeId){
-          openHours = employee.subsidiary.startingTime;
-          closeHours = employee.subsidiary.endTime;
+      this.employees.filter(employee => {
+        if (employee._id === employeeId) {
+          openHours = moment(
+            employee.subsidiary.startingTime,
+            "HH:mm"
+          ).format();
+          closeHours = moment(employee.subsidiary.endTime, "HH:mm").format();
         }
-      })
+      });
       console.log("Horario Sucursal", openHours, closeHours);
       //verifica si la fecha pasada es domingo
+      // debugger;
       if (date.getDay() === 0 || holiday !== -1) {
         result = "00:00";
         return result;
       } else {
-        //calculo basado en el horario de entrada 
+        //calculo basado en el horario de entrada
         result = moment(openHours).diff(entryTime, "minutes");
-        if(result < 0){
+        if (result < 0) {
           return this.convertToHours(result);
-        }else{
-          return null
+        } else {
+          return null;
         }
         //Calculo basado en la cantidad de horas que debe trabajar
         // workedHours = moment(exitTime).diff(entryTime, "minutes");
