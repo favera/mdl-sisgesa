3163e40c1ecda4fe45be1ac42d8eb274c36b4e66
favera <faa88ver@gmail.com>
2018-09-06 05:33:36 -0400

    screen users

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,64 +1,63 @@
 <template>
-<div class="ui twelve wide column">
-    <form class="ui form" @submit.prevent="saveUser">
-        <div class="ui dividing header">Registrar Usuario</div>
-        <div class="ui ten wide required field">
-            <label for="">Nombre y Apellido</label>
-            <div class="field">
-                <input type="text" v-model="user.name">
+    <div class="ui twelve wide column">
+        <form class="ui form" @submit.prevent="saveUser">
+            <div class="ui dividing header">Registrar Usuario</div>
+            <div class="ui ten wide required field">
+                <label for="">Nombre y Apellido</label>
+                <div class="field">
+                    <input type="text" v-model="user.name">
+                </div>
             </div>
-        </div>
 
-        <div class="ui ten wide required field">
-            <label for="">Nombre de Usuario</label>
-            <div class="field">
-                <input type="text" v-model="user.username">
+            <div class="ui ten wide required field">
+                <label for="">Nombre de Usuario</label>
+                <div class="field">
+                    <input type="text" v-model="user.username">
+                </div>
             </div>
-        </div>
 
-        <div class="ui ten wide required field">
-            <label for="">Email</label>
-            <div class="field">
-                <input type="email" name="" id="user-email" v-model="user.email" >
+            <div class="ui ten wide required field">
+                <label for="">Email</label>
+                <div class="field">
+                    <input type="email" name="" id="user-email" v-model="user.email">
+                </div>
             </div>
-        </div>
 
-        <div class="ui ten wide required field">
-            <label for="">Perfil del Usuario</label>
-            <div class="field">
-                <select class="ui fluid selection dropdown" v-model="user.profile">
-                <option disabled value="">Seleccionar perfil</option>
-                    <option value="Administrador">Administrador</option>
-                    <option value="Operador">Operardor</option>
-                    <option value="Consulta">Consulta</option>
-            </select>
+            <div class="ui ten wide required field">
+                <label for="">Perfil del Usuario</label>
+                <div class="field">
+                    <select class="ui fluid selection dropdown" v-model="user.profile">
+                        <option disabled value="">Seleccionar perfil</option>
+                        <option value="Administrador">Administrador</option>
+                        <option value="Operador">Operardor</option>
+                        <option value="Consulta">Consulta</option>
+                    </select>
+                </div>
+
             </div>
-            
-        </div>
 
-
-        <div class="ui five wide required field">
-            <label for="">Contraseña</label>
-            <div class="ui input" :class="{'action': true}">
-                <input class="ui disabled input" type="password" >
-                <button class="ui icon button" v-show="true">
+            <div class="ui five wide required field">
+                <label for="">Contraseña</label>
+                <div class="ui input" :class="{'action': edit}">
+                    <input ref="password" class="ui input deny-focus" :class="{'disabled': edit}" type="password">
+                    <div class="ui icon button" v-show="edit" @click="focus">
                         <i class="refresh icon"></i>
-                </button>
-            </div>
+                    </div>
+                </div>
 
-            <!-- <div class="field">
+                <!-- <div class="field">
                 <input type="password" name="" id="" v-model="user.password">
             </div> -->
-        </div>
+            </div>
 
-        <div class="field">
-            <button class="ui teal button" >Guardar</button>
-            <div class="ui button" @click="cancel">Cancelar</div>
-        </div>
+            <div class="field">
+                <button class="ui teal button">Guardar</button>
+                <div class="ui button" @click="cancel">Cancelar</div>
+            </div>
 
-    </form>
-</div>
-    
+        </form>
+    </div>
+
 </template>
 <script>
 export default {
@@ -71,12 +70,14 @@ export default {
         profile: null,
         password: null
       },
-      edit: false
+      edit: false,
+      focus: false
     };
   },
   methods: {
     getUser() {
       if (this.$route.params.id) {
+        this.edit = this.$route.params.edit;
         this.$http
           .get(`/users/edit/${this.$route.params.id}`)
           .then(response => {
@@ -99,6 +100,9 @@ export default {
       this.$http.post(`/users/add`, this.user).then(response => {
         console.log(response);
       });
+    },
+    focus() {
+      this.$refs.password.focus();
     }
   },
   mounted() {
@@ -112,6 +116,9 @@ export default {
 };
 </script>
 <style>
+.deny-focus {
+  pointer-events: none;
+}
 </style>
 
 
--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -4,27 +4,27 @@
             <h4 class="ui dividing header">Listado de Usuarios</h4>
             <div class="two fields">
                 <div class="inline fields">
-                <div class="ten wide field">
-                    <div class="ui icon input">
-                    <input  type="text" placeholder="Buscar Usuario..." >
-                    <i class="close link icon"></i>
+                    <div class="ten wide field">
+                        <div class="ui icon input">
+                            <input type="text" placeholder="Buscar Usuario...">
+                            <i class="close link icon"></i>
+                        </div>
                     </div>
-                </div>
 
-                <button class="ui circular teal icon button">
-                    <i class="search icon"></i>
-                </button>
+                    <button class="ui circular teal icon button">
+                        <i class="search icon"></i>
+                    </button>
 
                 </div>
 
                 <div class="field">
 
-                <div class="ui right floated main menu">
-                    <a class="icon item" >
-                    <i class="plus icon" @click="newUser"></i>
-                    </a>
-                    
-                </div>
+                    <div class="ui right floated main menu">
+                        <a class="icon item">
+                            <i class="plus icon" @click="newUser"></i>
+                        </a>
+
+                    </div>
 
                 </div>
             </div>
@@ -35,15 +35,15 @@
 
             <table class="ui teal striped celled table">
                 <thead>
-                <tr>
-                    <th >                   
-                    Nombre y Apellido
-                    </th>
-                    <th>Nombre de Usuario</th>
-                    <th>Perfil</th>
-                    <th>Email</th>
-                    <th class="center aligned">Opciones</th>
-                </tr>
+                    <tr>
+                        <th>
+                            Nombre y Apellido
+                        </th>
+                        <th>Nombre de Usuario</th>
+                        <th>Perfil</th>
+                        <th>Email</th>
+                        <th class="center aligned">Opciones</th>
+                    </tr>
                 </thead>
                 <tbody>
                     <tr v-for="(user, index) in users" :key="user._id">
@@ -53,25 +53,24 @@
                         <td>{{user.email}}</td>
                         <td class="center aligned">
                             <i class="edit icon" @click="editUser(user._id)"></i>
-                            <i class="trash icon"></i>
+                            <i class="trash icon" @click="deleteUser(user._id, index)"></i>
                         </td>
                     </tr>
-             
 
                 </tbody>
 
                 <tfoot>
-                <tr>
-                    <!-- <th colspan="7">
+                    <tr>
+                        <!-- <th colspan="7">
                     <app-pagination :current-page="pageOne.currentPage" :total-items="pageOne.totalItems" :items-per-page="pageOne.itemsPerPage" @page-changed="pageOneChanged">
                     </app-pagination>
                     </th> -->
-                </tr>
+                    </tr>
                 </tfoot>
             </table>
 
         </div>
-    </div>    
+    </div>
 </template>
 <script>
 export default {
@@ -97,6 +96,34 @@ export default {
       this.$http.get(`/users/users-list`).then(response => {
         this.users = response.data;
       });
+    },
+    deleteUser(userId, index) {
+      this.$confirm(
+        "Este registro sera eliminado permanentemente. Continuar?",
+        "Alerta",
+        {
+          confirmButtonText: "OK",
+          cancelButtonText: "Cancelar",
+          type: "warning"
+        }
+      )
+        .then(() => {
+          this.$http.delete(`/users/delete/${userId}`).then(response => {
+            if (response.status === 200) {
+              this.users.splice(index, 1);
+              this.$message({
+                type: "success",
+                message: "Registro Eliminado exitosamente"
+              });
+            }
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "Proceso cancelado"
+          });
+        });
     }
   },
   created() {
