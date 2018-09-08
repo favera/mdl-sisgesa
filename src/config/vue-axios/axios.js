10143ced82fe66ed2ca0afee47dd33e66ebd9676
mdldev <faa88ver@gmail.com>
2018-09-08 13:42:35 -0400

    test env var heroku

--- src/config/vue-axios/axios.js
+++ src/config/vue-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.API_URL || "http://localhost:3000";
-
+// const API_URL = process.env.API_URL || "http://localhost:3000";
+const API_URL = "https://sisgesa-api.herokuapp.com/";
 export default axios.create({
   baseURL: API_URL,
   transformRequest: [
